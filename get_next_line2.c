ad94862c0703e54039484996a93c513fc6113e96
Kaietan <34032203+kaietanpaul@users.noreply.github.com>
2024-04-20 21:05:41 +0000

    main - not printing last line

--- get_next_line2.c
+++ get_next_line2.c
@@ -1,13 +1,13 @@
 #include "utils.c"
 
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE 20
+# define BUFFER_SIZE 10
 #endif
 
 char *get_next_line(int fd)
 {
 	static char	*unsaved = NULL;
-	char	*unsaved_temp;
+	char	*unsaved_temp = NULL;
 	char	*buffer;
 	char	*printable = NULL;
 	int	buffer_read;
@@ -15,50 +15,65 @@ char *get_next_line(int fd)
 
 	buffer_read = 0;
 	i_new_line = 0;
-	buffer = malloc(BUFFER_SIZE);
+	buffer = malloc(BUFFER_SIZE + 1);
 	buffer_read = read(fd, buffer, BUFFER_SIZE);
+	buffer[buffer_read] = '\0';
 
-	if (buffer_read > 0)
+	if (buffer_read > -1)
 	{
 		if (unsaved)
 		{
 			unsaved_temp = ft_strjoin(unsaved, buffer);
 			free(unsaved);
-			unsaved = ft_strjoin(unsaved_temp, buffer);
+			unsaved = ft_strdup(unsaved_temp);
 			free(unsaved_temp);
 		}
 		else
 			unsaved = ft_strdup(buffer);
 	
 		i_new_line = find_new_line(unsaved);
+		while (i_new_line == -1 && buffer_read != 0)
+		{
+			buffer_read = read(fd, buffer, BUFFER_SIZE);
+			buffer[buffer_read] = '\0';
+			unsaved_temp = ft_strjoin(unsaved, buffer);
+			free(unsaved);
+			unsaved = ft_strdup(unsaved_temp);
+			free(unsaved_temp);
+			i_new_line = find_new_line(unsaved);
+		}
 		if (i_new_line > -1)
 		{
-			printable = ft_substr(unsaved, 0, i_new_line);
+			printable = ft_substr(unsaved, 0, i_new_line + 1);
+			unsaved_temp = ft_substr(unsaved, i_new_line + 1, ft_strlen(unsaved) + 1);
 			free(unsaved);
-			unsaved = ft_substr(buffer, i_new_line, ft_strlen(buffer));
+			unsaved = ft_strdup(unsaved_temp);
+			free(unsaved_temp);
+		}
+		else if (i_new_line == -1 && buffer_read == 0)
+		{
+			free(unsaved);
 		}
 	}
+	else
+		return (NULL);
 	free(buffer);
 	return (printable);
 }
 
-int	main(void)
-{
-	char	*str;
-	int	fd;
-	int i = 0;
 
-	fd = open("text01.txt", O_RDONLY);
-	while (i <= 50)
-	{
-		str = get_next_line(fd);
-		if (str)
-			printf("iteration :%d\n%s\n", i, str);
-		else
-			printf("\niteration :%d ------------------\n", i);
-		i++;
-		free(str);
+int main(void) {
+	int fd;
+	char *line;
+	fd = open("newline.txt", O_RDONLY);
+	if (fd == -1) {
+		perror("Error opening file");
+		return 1;
+	}
+	while ((line = get_next_line(fd)) != NULL) {
+		printf("%s\n", line);
+		free(line);
 	}
 	close(fd);
-	return (0);
+	return 0;
 }
